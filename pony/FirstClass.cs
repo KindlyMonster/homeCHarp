5e63de0afa61dd9cc3a3d0f362dfa33206f7b25c
Miftahov Vladislav <44448075+KindlyMonster@users.noreply.github.com>
2020-04-07 14:28:31 +0500

    Add files via upload

---
+++ pony/FirstClass.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace pony
+{
+    class FirstClass
+    {
+        private List<string> name = new List<string>();
+        private bool flagChek;//проверка элемента start
+        private void changeFlag()//меняю флаг и удаляю первое значение
+        {
+            name.Remove(name[0]);
+            flagChek = !flagChek;
+        }
+        public FirstClass()//пустой конструктор
+        {
+            List<string> name = new List<string>() { "start" };//поле с 1 элементом что работало
+            flagChek = true;
+        }
+        public FirstClass(string[] strArr)//конструктор с массивом
+        {
+            List<string> name0 = strArr.OfType<string>().ToList();//преобразуем массив в список
+            for (int i = 0; i < name0.Count; i++)
+                if (!name.Contains(name0[i]))//ищу совпадения в списке
+                    name.Add(name0[i]);//добавляем в список
+            changeFlag();
+        }
+        public FirstClass(List<string> listName)//конструктор со списком
+        {
+            name.AddRange(listName);
+            changeFlag();
+        }
+        public void addList(List<string> listName)//добавление списка
+        {
+            for (int i = 0; i < listName.Count; i++)
+                if (!name.Contains(listName[i]))//ищу совпадения в списке
+                    name.Add(listName[i]);//добавляем в список
+        }
+        public static FirstClass operator +(FirstClass name1, FirstClass name2)//перегрузка оператора сложения
+        {
+            FirstClass uno2List = new FirstClass(name1.name.ToArray());
+            uno2List.addList(name2.name);
+            return uno2List;
+        }
+        public override string ToString()
+        {
+            if (flagChek)
+            {
+                string allList = "";
+                foreach (string elem in name)
+                    allList += elem + "\n";
+                return allList;
+            }
+            else
+                return "ErrorEmptyList"; //обработка если список пустой
+        }
+        public string minElemList()//поиск минимального элемента
+        {
+            string minElem = "";
+            int count = int.MaxValue;
+            foreach (string elem in name)
+            {
+                if (count > elem.Length)
+                {
+                    count = elem.Length;
+                    minElem = elem + "\n";
+                }
+            }
+            return minElem;
+        }
+        public string getElem2Index(int index)//поиск по индексу
+        {
+            if (index < name.Count)
+                return name.ElementAt(index);
+            else
+                return "OutOfBounds";
+        }
+    }
+}
---
+++ pony/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArr = { "pip", "w" , "pop", "pip", "we", "pop", "wqe", "1", "1", "wqrewfdsc", "ewqfdsvdfbserb" };
+            string[] inputArr1 = { "pop", "ываw", "аыв", "мсчмв", "ы", "выфц", "pip", "666", "41", "asfewvdvrew" , "d123rf4re" };
+
+            FirstClass name = new FirstClass(inputArr);
+            FirstClass name1 = new FirstClass(inputArr1);
+
+            Console.WriteLine("List1 \n{0}",name);
+            Console.WriteLine("List2 \n{0}", name1);
+
+            Console.WriteLine("List1 + List2 \n{0}", name1 + name);
+
+            Console.WriteLine("Return min elem in list1 \n{0}", name.minElemList());
+            Console.WriteLine("Return min elem in list2 \n{0}", name1.minElemList());
+
+            Console.WriteLine("Return an elem by index in list1 \n{0}", name.getElem2Index(5));
+            Console.ReadKey();
+        }
+    }
+}
